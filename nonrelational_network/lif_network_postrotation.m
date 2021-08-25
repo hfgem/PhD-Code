5225e93c65e4f61d24301e269c8f57bed3a04023
Hannah Germaine <hfgermaine@gmail.com>
2021-08-25 11:06:19 -0400

    Update lif_network_postrotation.m
    Updated parameters, updated data storage, added spike event differentiation and event visualization

--- nonrelational_network/lif_network_postrotation.m
+++ nonrelational_network/lif_network_postrotation.m
@@ -15,9 +15,9 @@ cluster_n = round(n/5); %number of neurons in a cluster (for small n round(n/3),
 t_max = 2; %maximum amount of time (s)
 dt = 0.1*10^(-3); %timestep (s)
 t_steps = t_max/dt; %number of timesteps in simulation
-tau_syn_E = 20*10^(-3); %AMPA/NMDA synaptic decay time constant (s)
+tau_syn_E = 10*10^(-3); %AMPA/NMDA synaptic decay time constant (s)
 tau_syn_I = 5*10^(-3); %GABA synaptic decay time constant (s)
-tau_sra = 250*10^(-3); %spike rate adaptation time constant (s)
+tau_sra = 30*10^(-3); %spike rate adaptation time constant (s)
 E_K = -80*10^(-3); %potassium reversal potential (V) %-75 or -80 mV
 E_L = -70*10^(-3); %leak reversal potential (V) %-60 - -70 mV range
 G_L = 25*10^(-9); %leak conductance (S) %10 - 30 nS range
@@ -29,26 +29,26 @@ V_syn_I = -70*10^(-3); %synaptic reversal potential (inhibitory) %generally -70 
 E_syn_E = V_syn_E*ones(n,1); %vector of the synaptic reversal potential for excitatory connections
 E_syn_I = V_syn_I*ones(n,1); %vector of the synaptic reversal potential for inhibitory connections
 %______Split del_G_syn______
-del_G_syn_E = 10*10^(-9); %synaptic conductance step following spike (S)
-del_G_syn_I = 10*10^(-9); %synaptic conductance step following spike (S)
+del_G_syn_E = 8*10^(-9); %synaptic conductance step following spike (S)
+del_G_syn_I = 8*10^(-9); %synaptic conductance step following spike (S)
 %___________________________
 del_G_sra = 200*10^(-9); %spike rate adaptation conductance step following spike %ranges from 1-200 *10^(-9) (S)
 connectivity_gain = 1; %amount to increase or decrease connectivity by with each spike (more at the range of 1.002-1.005)
-IEI = 0.5; %inter-event-interval (s) the elapsed time between spikes to count separate events
+IEI = 0.1; %inter-event-interval (s) the elapsed time between spikes to count separate events
 IES = ceil(IEI/dt); %inter-event-steps = the number of steps to elapse between spikes
 
 %How spikes are initiated:
 %'cluster' sets a cluster to threshold;
 %'current' means spikes depend on an input current of I_in; 
 %'neuron' sets a random selection of 2% of neurons to threshold
-type = 'cluster'; %'current'; %'neuron';
+type = 'current'; %'neuron'; %'cluster';
 
 %How many tests of different initializations to run
 if strcmp(type,'cluster')
     test_val_max = clusters; %Ensures every cluster is initialized
 else
     test_val_max = 10; %This value can be modified as you see fit
-end   
+end
 
 %Adding an input current to all cells (one of the options must be uncommented)
 x_in = [0:dt:t_max];
@@ -57,7 +57,7 @@ x_in = [0:dt:t_max];
 % I_Hz = 1; %frequency of input - 1 Hz or 60 bpm, 4-8 Hz for theta rhythm
 % I_in = I_coeff*(0.5 + 0.5*sin(I_Hz*2*pi*x_in)); %Approximately theta wave input current
 %Noisy input: (uncomment if desired)
-I_coeff = 0; %set to 0 for no input current
+I_coeff = 2.7; %set to 0 for no input current
 I_scale = 1*10^(-9); %sets the scale of the current input
 I_in = I_coeff*randn(n,t_steps+1)*I_scale; %Generally use -0.5-0.5 nA stimulus
 
@@ -69,31 +69,23 @@ cluster_prob = min(conn_prob*npairs/nclusterpairs,1); %intra-cluster connection 
 p_E = 0.75; %probability of an excitatory neuron
 p_I = 1 - p_E; %probability of an inhibitory neuron
 n_I = round(p_I*n); %number of inhibitory neurons
-all_indices = [1:n];
 
-%OPTIONAL: Save parameters to a .txt file (note, must have empty Workspace
-%befor running this section for this to work)
-param_names = who;
-fileID = fopen(strcat(save_path,'/param_file.txt'), 'wt'); % Open and create file in text writing mode.
-fprintf(fileID, 'Parameters:\r\n');
-fprintf(fileID, ' =======================================\r\n\r\n\r\n');
-for i = 1:length(param_names)
-    value = eval(param_names{i});
-    [a,b] = size(value);
-    if strcmp(class(value),'double') && a*b == 1 %#ok<STISA> %Only saves doubles of single value
-        fprintf(fileID, strcat(param_names{i},'\t%s\r\n'), sprintf('%0.5e',value)); %write the variable
-    elseif strcmp(param_names{i},'type')
-        fprintf(fileID, strcat(param_names{i},'\t%s\r\n'), value); %write the variable
-    end
-    clear value a b
+%Save parameters to a structure and to computer
+w = whos;
+parameters = struct;
+for a = 1:length(w) 
+    parameters.(w(a).name) = eval(w(a).name); 
 end
-clear i
-fclose(fileID);
+clear w a
+save(strcat(save_path,'/parameters.mat'))
 
 %% Create networks and test spike progressions
 %Runs through a series of different random number generator seeds to change
 %the network connectivity and setup, and then automatically outputs
 %sequence data to a folder
+
+%If uploading a parameter file, uncomment the next line
+load(strcat(save_path,'/parameters.mat'))
 
 for i = 1:10 %how many different network structures to test
     rng(i) %set random number generator for network structure
@@ -105,18 +97,20 @@ for i = 1:10 %how many different network structures to test
     end
     
     %SET UP NETWORK
-    [cluster_mat, conns] = create_clusters(n, clusters, cluster_n, ...
-        cluster_prob);
+    [cluster_mat, conns] = create_clusters(parameters.n, ...
+        parameters.clusters, parameters.cluster_n, parameters.cluster_prob);
     conns_copy = conns; %just a copy of the connections to maintain for reset runs if there's "plasticity"
     %Randomize excitatory and inhibitory connection strengths based on selected
     %probability.
-    I_indices = randi(n,[n_I,1]); %indices of inhibitory neurons
+    all_indices = [1:parameters.n];
+    I_indices = randi(parameters.n,[parameters.n_I,1]); %indices of inhibitory neurons
     all_indices(I_indices) = 0;
     E_indices = find(all_indices)'; %indices of excitatory neurons
     n_EE = sum(conns(E_indices,E_indices),'all'); %number of E-E connections
     n_EI = sum(conns(E_indices,I_indices),'all'); %number of E-I connections
     n_II = sum(conns(I_indices,I_indices),'all'); %number of I-I connections
     n_IE = sum(conns(I_indices,E_indices),'all'); %number of I-E connections
+    clear all_indices
     
     %SAVE NETWORK STRUCTURE
     network = struct;
@@ -131,108 +125,130 @@ for i = 1:10 %how many different network structures to test
     %Run through every cluster initialization and store relevant data and
     %calculations
     network_var = struct;
-    network_spike_sequences = zeros(clusters,n);
-    network_spike_ranks = zeros(clusters,n);
-    non_spiking_neurons = zeros(clusters,n);
+    network_spike_sequences = struct;
     network_cluster_sequences = struct; 
     
-    for j = 1:test_val_max
+    for j = 1:parameters.test_val_max
         seed = j;
         
         %Create Storage Variables
-        I_syn = zeros(n,t_steps+1); %synaptic current emitted by each neuron at each timestep (A)
+        I_syn = zeros(parameters.n,parameters.t_steps+1); %synaptic current emitted by each neuron at each timestep (A)
         %synaptic conductances for each neuron at each timestep
-        G_syn_I = zeros(n,t_steps+1); %conductance for presynaptic inhibitory (S)
-        G_syn_E = zeros(n,t_steps+1); %conductance for presynaptic excitatory (S)
-        V_m = zeros(n,t_steps+1); %membrane potential for each neuron at each timestep
-        V_m(:,1) = V_reset + randn([n,1])*(10^(-3)); %set all neurons to baseline reset membrane potential with added noise
-        G_sra = zeros(n,t_steps+1); %refractory conductance for each neuron at each timestep
+        G_syn_I = zeros(parameters.n,parameters.t_steps+1); %conductance for presynaptic inhibitory (S)
+        G_syn_E = zeros(parameters.n,parameters.t_steps+1); %conductance for presynaptic excitatory (S)
+        V_m = zeros(parameters.n,parameters.t_steps+1); %membrane potential for each neuron at each timestep
+        V_m(:,1) = parameters.V_reset + randn([parameters.n,1])*(10^(-3)); %set all neurons to baseline reset membrane potential with added noise
+        G_sra = zeros(parameters.n,parameters.t_steps+1); %refractory conductance for each neuron at each timestep
         
         %Run model
-        [V_m, G_sra, G_syn_I, G_syn_E, I_syn] = lif_sra_calculator_postrotation(n, ...
-            cluster_mat,conns,V_m,V_reset,V_th,G_sra,del_G_sra,...
-            G_syn_I, G_syn_E, del_G_syn_E,del_G_syn_I,I_syn,E_syn_E,E_syn_I,E_K,E_L,...
-            G_L,C_m,dt,tau_sra,tau_syn_E,tau_syn_I,connectivity_gain,...
-            I_indices,E_indices,I_in,seed,t_steps,type);
+        [V_m, G_sra, G_syn_I, G_syn_E, I_syn] = lif_sra_calculator_postrotation(...
+            parameters, seed, cluster_mat, conns, I_indices, E_indices, ...
+            I_syn, G_syn_I, G_syn_E, V_m, G_sra);
         network_var(j).V_m = V_m;
         network_var(j).G_sra = G_sra;
         network_var(j).G_syn_I = G_syn_I;
         network_var(j).G_syn_E = G_syn_E;
         network_var(j).I_syn = I_syn;
         
-        %Find spike sequence
-        spikes_V_m = V_m >= V_th;
+        %Find spike profile
+        spikes_V_m = V_m >= parameters.V_th;
         [spikes_x,spikes_t] = find(spikes_V_m);
         max_time = max(spikes_t);
-        spike_order = unique(spikes_x,'stable');
-        for k = 1:length(spike_order)
-            n_ind = spike_order(k);
-            network_spike_ranks(j,n_ind) = k;
-        end    
-        for k = 1:n %make sure all indices are accounted for (non-spiking neurons)
-            if isempty(find(spike_order == k,1))
-                spike_order(end+1) = k; %#ok<SAGROW>
-                non_spiking_neurons(j,k) = 1;
-            end
-        end
-        network_spike_sequences(j,:) = spike_order;
-        clear k spikes_x
         
         %Find maximum firing rate + average maximum firing rates of neurons
-        all_fr = sum(spikes_V_m,2)/t_max;
+        all_fr = sum(spikes_V_m,2)/parameters.t_max;
         max_fr = max(all_fr);
         avg_fr = mean(all_fr);
-        %USE THIS AS AN INDICATOR OF GOOD SEQUENCES, RATHER THAN VISUALIZING
+        
+        %USE FR AS AN INDICATOR OF GOOD SEQUENCES
         %In hippocampus, Jadhav lab uses 3 Hz as a place-cell cutoff
-        if and(max_fr <= 3, max_fr >= 1) || and(avg_fr <= 3, avg_fr >= 0.5)
-            %Visualize re-ordered spike sequence
-            reordered_spikes = zeros(size(spikes_V_m));
-            reordered_V_m = zeros(size(V_m));
-            for i = 1:n
-                reordered_spikes(i,:) = spikes_V_m(spike_order(i),:);
-                reordered_V_m(i,:) = V_m(spike_order(i),:);
-            end
-            f = figure;
-            imagesc(reordered_spikes(:,1:max(spikes_t)))
-            xticks(round(linspace(1,max(spikes_t),10)))
-            xt = get(gca, 'XTick');
-            xtlbl = round(linspace(0, t_max, numel(xt)),2);
-            colormap(flip(gray))
-            if strcmp(type,'cluster')
-                title(strcat('Spiking Behavior: cluster = ',string(j)),'FontSize',16)
-            else
-                title('Spiking Behavior','FontSize',16)
-            end
-            xlabel('Time (s)','FontSize',16)
-            ylabel('Reordered Neuron Number','FontSize',16)
-            set(gca, 'XTick',xt, 'XTickLabel',xtlbl)
-            savefig(f,strcat(net_save_path,'/',type,'_',string(j),'firing_sequence.fig'))
-            saveas(f,strcat(net_save_path,'/',type,'_',string(j),'firing_sequence.jpg'))
-            close(f)
-            clear i f
-
+        if and(max_fr <= 3, max_fr >= 1) && and(avg_fr <= 2, avg_fr >= 1/(parameters.t_max+1))
             %Find event times
             events = []; 
             last_start = spikes_t(1);
             last_time = spikes_t(1);
+            spike_count = 1;
             for t_i = 2:length(spikes_t)
                 s_i = spikes_t(t_i);
-                if s_i - last_time <= IES
-                    last_time = s_i; %update the last time to be this one
+                if s_i - last_time <= parameters.IES
+                    last_time = s_i;
+                    spike_count = spike_count + 1;
                 else
-                    events = [events; [last_start, last_time]]; %#ok<AGROW> %add the last range of spikes to the events vector
+                    if (last_start ~= last_time) && (spike_count > 0.05*parameters.n) %weed out events of single spikes
+                        events = [events; [last_start, last_time]]; %#ok<AGROW> %add the last range of spikes to the events vector
+                    end
                     last_start = s_i;
                     last_time = s_i;
+                    spike_count = 1;
                 end
             end
             if last_start ~= last_time %if the last event is a single spike, we don't care about it
                 events = [events; [last_start, last_time]]; %#ok<AGROW> %add the last interval
             end
             [num_events,~] = size(events);
+            display(events)
+            %save to both structures
+            network_spike_sequences(j).events = events;
+            network_cluster_sequences(j).events = events;
+
+            %Find spike sequences
+            for e_i = 1:num_events
+                %store spike orders for each event
+                event_spikes = spikes_V_m(:,events(e_i,1):events(e_i,2));
+                [e_spikes_x, ~] = find(event_spikes);
+                spike_order = unique(e_spikes_x,'stable');
+                network_spike_sequences(j).spike_order.(strcat('sequence_',string(e_i))) = spike_order;
+                %store ranks for each neuron
+                ranks_vec = zeros(1,parameters.n);
+                for k = 1:length(spike_order)
+                    n_ind = spike_order(k);
+                    ranks_vec(1,n_ind) = k;
+                end
+                network_spike_sequences(j).spike_ranks.(strcat('sequence_',string(e_i))) = ranks_vec;
+                %store nonspiking neurons
+                nonspiking_neurons = isnan(ranks_vec./ranks_vec);
+                network_spike_sequences(j).nonspiking_neurons.(strcat('sequence_',string(e_i))) = nonspiking_neurons;
+            end
+            clear e_i event_spikes e_spikes_x spike_order ranks_vec k n_ind nonspiking_neurons
+            
+            %Visualize re-ordered spike sequences
+            f = figure;
+            axes = [];
+            for e_i = 1:num_events
+                spike_order = network_spike_sequences(j).spike_order.(strcat('sequence_',string(e_i)));
+                sub_spikes_V_m = spikes_V_m(:,events(e_i,1):events(e_i,2));
+                reordered_spikes = zeros(size(sub_spikes_V_m));
+                [~,event_length] = size(reordered_spikes);
+                for s_i = 1:length(spike_order)
+                    reordered_spikes(s_i,:) = sub_spikes_V_m(spike_order(s_i),:);
+                end 
+                ax = subplot(1,num_events,e_i);
+                axes = [axes, ax];
+                imagesc(reordered_spikes)
+                xticks(round(linspace(1,event_length,20))) %20 ticks will be displayed
+                xt = get(gca,'XTick');
+                xtlbl = round(linspace(events(e_i,1)*parameters.dt,events(e_i,2)*parameters.dt,numel(xt)),2);
+                colormap(flip(gray))
+                xlabel('Time (s)','FontSize',16)
+                ylabel('Reordered Neuron Number','FontSize',16)
+                title(strcat('Event #',string(e_i)))
+                set(gca, 'XTick',xt, 'XTickLabel',xtlbl)
+            end
+            if strcmp(type,'cluster')
+                sgtitle(strcat('Spiking Behavior: cluster = ',string(j)),'FontSize',16)
+            else
+                sgtitle('Spiking Behavior','FontSize',16)
+            end
+            %linkaxes(axes)
+            savefig(f,strcat(net_save_path,'/',type,'_',string(j),'firing_sequence.fig'))
+            saveas(f,strcat(net_save_path,'/',type,'_',string(j),'firing_sequence.jpg'))
+            close(f)
+            clear e_i spike_order reordered_spikes event_length s_i ax ...
+                axes xt xtlbl
             
             %Find cluster sequence per event by moving bin
             bin_width = 5*10^(-3); %5 ms bin
-            bin_size = ceil(bin_width/dt); %number of timesteps to use in a bin
+            bin_size = ceil(bin_width/parameters.dt); %number of timesteps to use in a bin
             for e_i = 1:num_events
                 cluster_spikes = cluster_mat*spikes_V_m(:,events(e_i,1):events(e_i,2));
                 cluster_mov_sum = movsum(cluster_spikes',bin_size)';
@@ -240,9 +256,10 @@ for i = 1:10 %how many different network structures to test
                 normalized_cluster_spikes(isnan(normalized_cluster_spikes)) = 0;
                 normalized_cluster_mov_sum = cluster_mov_sum ./ sum(cluster_mov_sum,1);
                 normalized_cluster_mov_sum(isnan(normalized_cluster_mov_sum)) = 0;
-                network_cluster_sequences(j).clusters.(strcat('cluster_',string(e_i))) = cluster_spikes;
-                network_cluster_sequences(j).movsum.(strcat('cluster_',string(e_i))) = cluster_mov_sum;
-                network_cluster_sequences(j).normalized_clusters.(strcat('cluster_',string(e_i))) = normalized_cluster_spikes;
+                network_cluster_sequences(j).clusters.(strcat('sequence_',string(e_i))) = cluster_spikes;
+                network_cluster_sequences(j).movsum.(strcat('sequence_',string(e_i))) = cluster_mov_sum;
+                network_cluster_sequences(j).normalized_clusters.(strcat('sequence_',string(e_i))) = normalized_cluster_spikes;
+                network_cluster_sequences(j).normalized_cluster_mov_sum.(strcat('sequence_',string(e_i))) = normalized_cluster_mov_sum;
             end
             clear bin_width bin_size cluster_spikes cluster_mov_sum e_i
         end
@@ -250,10 +267,8 @@ for i = 1:10 %how many different network structures to test
     
     %SAVE NETWORK DATA
     save(strcat(net_save_path,'/network_var.mat'),'network_var','-v7.3')
-    save(strcat(net_save_path,'/network_spike_sequences.mat'),'network_spike_sequences')
-    save(strcat(net_save_path,'/network_spike_ranks.mat'),'network_spike_ranks')
-    save(strcat(net_save_path,'/non_spiking_neurons.mat'),'non_spiking_neurons')
-    save(strcat(net_save_path,'/network_cluster_sequences.mat'),'network_cluster_sequences')
+    save(strcat(net_save_path,'/network_spike_sequences.mat'),'network_spike_sequences','-v7.3')
+    save(strcat(net_save_path,'/network_cluster_sequences.mat'),'network_cluster_sequences','-v7.3')
 end
 
 %% Calculate trajectory similarity
@@ -281,7 +296,7 @@ std_rank_mod = std(ranks_mod(ranks_mod~=1),[],'all');
 %This code block visualizes the sequence of clusters a particular spike
 %sequence progresses through.
 
-%Select and load data to analyze
+%Select and load specific network data to analyze
 net_save_path = uigetdir('/Users/hannahgermaine/Documents/PhD/','Select Network Save Folder'); %Have user input where they'd like the output stored
 load(strcat(net_save_path,'/network_spike_sequences.mat'))
 load(strcat(net_save_path,'/network_cluster_sequences.mat'))
@@ -294,6 +309,8 @@ spikes_V_m = V_m >= -50*10^(-3);
 [clusters,~] = size(network_spike_sequences); %Grab data sizes
 
 %Visualize cluster sequences
+seq_type = 'clusters'; %'movsum'; %'normalized_clusters';
+
 f3 = figure;
 subplot_n = sqrt(clusters);
 if floor(subplot_n)==subplot_n %assume non-infinite
